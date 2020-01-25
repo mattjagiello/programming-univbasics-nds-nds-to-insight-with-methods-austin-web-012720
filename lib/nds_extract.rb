b9f5ba33daab10f88d440db5b82bfc8356faf2ca
Matthew Jagiello <mjagiello@gmail.com>
2020-01-25 03:00:57 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -23,7 +23,7 @@ def directors_totals(nds)
   result = {}
   nds.each do |value|
     value.each do |key, value1|
-      result[:name] = value1 if key == :name
+      pp key
     end
     puts result
   end
