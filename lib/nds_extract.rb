2a7c2ead13082e453bb830b41febc1164f0869aa
Matthew Jagiello <mjagiello@gmail.com>
2020-01-25 02:23:21 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -15,7 +15,7 @@ def gross_for_director(director_data)
   array = []
   director_data[:movies].each do |value|
     value.each do |key, value1|
-        puts value1
+        array << [key, value1]
     end
   end
   return array
